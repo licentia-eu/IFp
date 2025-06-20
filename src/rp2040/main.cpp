e9d1a8456ef310780087d58c3b7a9011c5257e80
BogDan Vatra <bogdan@kde.org>
2025-06-20 12:07:33 +0300

    add adc

--- src/rp2040/main.cpp
+++ src/rp2040/main.cpp
@@ -19,6 +19,7 @@
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 // -----------------------------------------------------------------------------
 
+#include <hardware/adc.h>
 #include <hardware/gpio.h>
 #include <hardware/pio.h>
 #include <pico/stdlib.h>
@@ -72,6 +73,18 @@ int main()
     }
     puts("\033[2J\033[H");
 #endif
+
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+
+    while (1) {
+        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+        const float conversion_factor = 3.3f / (1 << 12);
+        uint16_t result = adc_read();
+        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
+        sleep_ms(500);
+    }
 
     for (int pin = 0; pin < 30; ++pin) {
         gpio_init(pin);
